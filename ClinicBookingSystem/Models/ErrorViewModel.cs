57450a9afdd28a20ff9ea519026b47fcd3ebe472
yasser-mohamed1 <y4ssermoh4med@gmail.com>
2025-11-11 00:37:04 +0200

    Add ErrorViewModel class for error handling

---
+++ ClinicBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClinicBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
